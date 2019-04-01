c304d8fc10753702261d597dcc8ff1dad1805a00
Garrett Luskey <garrett.luskey@gmail.com>
2019-04-01 15:34:43 -0500

    Created docstrings for Board class and methods

--- board.py
+++ board.py
@@ -5,12 +5,20 @@ import math
 
 
 class Board:
+    '''Board State Class
+       This class stores the state of a 8-puzzle by using
+       a 2d array to store the state of the game and having
+       methods to manipulate the array.
+    '''
     __BOARD_WIDTH = 3
     __BOARD_HEIGHT = 3
     __SCRAMBLE_ITERATIONS = 1000
     completed_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
     def __init__(self, board=None):
+        '''Board constructor
+           If no board is given, the constuctor will create a random board
+        '''
         if not board:
             self.board = self.completed_board
             return
@@ -32,6 +40,9 @@ class Board:
         return value
 
     def __check_board_valid(self, board):
+        ''' Verifies if given board is valid.
+            Returns true if board is valid, false if not.
+        '''
         # check if board is list and is the proper width
         if type(board) != list or len(board) != self.__BOARD_HEIGHT:
             return False
@@ -54,6 +65,9 @@ class Board:
         return True
 
     def __count_inversions(self):
+        ''' Counts inversions for if board is solvable.
+            Returns number of inversions.
+        '''
         inversion_count = 0
         for i in range(self.__BOARD_WIDTH * self.__BOARD_HEIGHT - 1):
             for j in range(i + 1, self.__BOARD_WIDTH * self.__BOARD_HEIGHT):
@@ -64,16 +78,23 @@ class Board:
         return inversion_count
 
     def __is_solvable(self):
+        ''' Verifies if the board is solvable.'''
         return self.__count_inversions() % 2 == 0
 
     def scramble(self):
+        ''' Scrambles board randomly.'''
         for x in range(self.__SCRAMBLE_ITERATIONS):
             self.board = random.choice(self.calculate_child_states()).board
 
     def get_current_state(self):
+        ''' Returns a copy of the board array.'''
         return copy.deepcopy(self.board)
 
     def move_up(self, cursor=None):
+        ''' Moves the empty block 1 place up.
+            Returns None if the empty block is on the top.
+            Otherwise returns new Board object with move.
+        '''
         if cursor:
             x, y = cursor
         else:
@@ -86,6 +107,10 @@ class Board:
         return Board(board)
 
     def move_down(self, cursor=None):
+        ''' Moves the empty block 1 place down.
+            Returns None if the empty block is on the bottom.
+            Otherwise returns new Board object with move.
+        '''
         if cursor:
             x, y = cursor
         else:
@@ -97,7 +122,11 @@ class Board:
         board[y - 1][x] = 0
         return Board(board)
 
-    def move_left(self, cursor=None):
+    def move_right(self, cursor=None):
+        ''' Moves the empty block 1 place to the right.
+            Returns None if the empty block is on the far right.
+            Otherwise returns new Board object with move.
+        '''
         if cursor:
             x, y = cursor
         else:
@@ -109,7 +138,11 @@ class Board:
         board[y][x + 1] = 0
         return Board(board)
 
-    def move_right(self, cursor=None):
+    def move_left(self, cursor=None):
+        ''' Moves the empty block 1 place to the left.
+            Returns None if the empty block is on the far left.
+            Otherwise returns new Board object with move.
+        '''
         if cursor:
             x, y = cursor
         else:
@@ -122,6 +155,7 @@ class Board:
         return Board(board)
 
     def __find_cursor(self):
+        ''' Return the x, y coordinates of the empty position on the board.'''
         for y in range(self.__BOARD_HEIGHT):
             for x in range(self.__BOARD_WIDTH):
                 if self.board[y][x] == 0:
@@ -129,6 +163,7 @@ class Board:
         return None
 
     def num_misplaced(self):
+        ''' Returns number of tiles that are not in the solved position.'''
         count = 0
         for i, x in enumerate(self.board):
             for j in range(len(x)):
@@ -137,12 +172,14 @@ class Board:
         return count
 
     def is_won(self):
+        ''' Returns if the game is in a completed state.'''
         if self.board == Board.completed_board:
             return True
         else:
             return False
 
     def calculate_child_states(self):
+        ''' Returns a list of Board objects for each valid move from given state.'''
         cursor = self.__find_cursor()
         return list(filter(None, [self.move_up(cursor),
                                   self.move_down(cursor),
