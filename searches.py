88a577f6243e69bbbb97de071b86571e86eea82a
mulrooneyju <mulrooneyju@uwplatt.edu>
2019-04-01 15:05:53 -0500

    Finished tracing route for the goal puzzle

--- searches.py
+++ searches.py
@@ -1,92 +1,65 @@
 from board import Board
+from queue import PriorityQueue
 from math import sqrt, floor
-
-
-def bfs(initial_state):
-    done = False
-    open_list = []
-    closed_list = []
-    temp = initial_state
-    while not done:
-        if temp.board == Board.completed_board:
-            closed_list.append(temp)
-            return closed_list
-        else:
-            closed_list.append(temp)
-            for x in temp.calculate_child_states():
-                open_list.append(x)
-            open_list = [x for x in open_list if x not in closed_list]  # removes lists that have moves already done
-            temp = open_list.pop(0)
-
+import time
 
 def greedy_search(initial_state):
-    queue = []
+    queue = PriorityQueue()
     visited = []
-    queue.append(HeuristicNode(calculate_heuristic_value(initial_state.board), initial_state))
-    while queue:
-        current_node = get_smallest_and_remove(queue)
-        if current_node.item.board in visited:
+    queue.put(HeuristicNode(calculate_heuristic_value(initial_state.board), initial_state))
+    while not queue.empty():
+        currentNode = queue.get()
+        if currentNode.item.board in visited:
             continue
-        visited.append(current_node.item.board)
-        if current_node.item.board == Board.completed_board:
+        visited.append(currentNode.item.board)
+        if currentNode.item.board == Board.completed_board:
             path = []
-            while current_node:
-                path.insert(0, current_node.item)
-                current_node = current_node.parent
+            while currentNode != None:
+                path.insert(0, currentNode.item)
+                currentNode = currentNode.parent
             return path
-        for child_state in current_node.item.calculate_child_states():
-            queue.append(HeuristicNode(calculate_heuristic_value(child_state.board), child_state, current_node))
-    return []
+        for child_state in currentNode.item.calculate_child_states():
+            if child_state.board not in visited: #check this again here because queue get and put is expensive
+                queue.put(HeuristicNode(calculate_heuristic_value(child_state.board), child_state, currentNode))
+    raise Exception("ouch")
 
+def bfs(initial_state, timeout=30):
+    initial_time = time.time()
+    openList = []
+    temp = (initial_state, None)
+    final = None
+    while not final and (time.time() - initial_time) < timeout:
+        if(temp[0].board == Board.completed_board):
+            final = temp
+        else:
+            for x in temp[0].calculate_child_states():
+                openList.append((x, temp))
+            temp = openList.pop(0)
 
-def a_star_search(initial_state):
-    queue = []
-    visited = []
-    queue.append(HeuristicNode(calculate_heuristic_value(initial_state.board), initial_state))
-    while queue:
-        current_node = get_smallest_and_remove(queue)
-        if current_node.item.board in visited:
-            continue
-        visited.append(current_node.item.board)
-        if current_node.item.board == Board.completed_board:
-            path = []
-            while current_node:
-                path.insert(0, current_node.item)
-                current_node = current_node.parent
-            return path
-        for child_state in current_node.item.calculate_child_states():
-            queue.append(
-                HeuristicNode(calculate_heuristic_value(child_state.board) + current_node.depth + 1, child_state,
-                              current_node, current_node.depth + 1))
-    return[]
+    if ((time.time() - initial_time) < timeout):
+        return -1
 
+    final_arr = []
+    while final[1] != None:
+        final_arr.append(final[0])
+        final = final[1]
 
-def get_smallest_and_remove(queue):
-    if not queue:
-        return None
-    index = 0
-    current_node = queue[0]
-    for i in range(len(queue)):
-        if queue[i].heuristic < current_node.heuristic:
-            current_node = queue[i]
-            index = i
-    del queue[index]
-    return current_node
+    final_arr.reverse()
+    for x in final_arr:
+        print(x)
 
 
 class HeuristicNode:
-    def __init__(self, heuristic, item, parent=None, depth=0):
+    def __init__(self, heuristic, item, parent=None):
         self.heuristic = heuristic
         self.item = item
         self.parent = parent
-        self.depth = depth
 
     def __gt__(self, heuristic_node):
         return self.heuristic > heuristic_node.heuristic
-
+ 
     def __lt__(self, heuristic_node):
         return self.heuristic < heuristic_node.heuristic
-
 
 def calculate_heuristic_value(board):
     heuristic_sum = 0
@@ -95,7 +68,8 @@ def calculate_heuristic_value(board):
             val = board[y][x]
             if val == 0:
                 val = 9
-            goal_x = (val - 1) % 3
-            goal_y = floor((val - 1) / 3)
-            heuristic_sum += sqrt((goal_x - x) ** 2 + (goal_y - y) ** 2)  # distance from goal state
+            goalx = (val - 1) % 3
+            goaly = floor((val - 1) / 3)
+            heuristic_sum += sqrt((goalx - x)**2 + (goaly - y)**2) # distance from goal state
     return heuristic_sum
+
